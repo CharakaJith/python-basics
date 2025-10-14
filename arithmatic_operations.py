e2d4300c06b065e8ea79497e79a799fb71e94377
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-10-14 23:54:06 +0530

    python arithmatic operations

---
+++ arithmatic_operations.py
@@ -0,0 +1,26 @@
+friends = 0
+
+# operation: addition
+# friends = friends + 5
+friends += 5 ## augmented assignment operator
+
+# operation: substraction
+# friends = friends - 1
+friends -= 1
+
+# operation: multiplication
+# friends = friends * 3
+friends *= 3
+
+# operation: division
+# friends = friends / 2
+friends /= 2
+
+# operation: exponent
+# friends = friends ** 2
+friends **= 2
+
+# operation: modulus
+reminder = friends % 8 ## how many remains if friends are divided to groups of 8
+
+print(f"you currently have {friends} friends")
