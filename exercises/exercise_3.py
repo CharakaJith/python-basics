b6daf293926494597cb1780350a8e53976e808dd
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-10-14 23:42:18 +0530

    exercise 3 completed

---
+++ exercises/exercise_3.py
@@ -0,0 +1,28 @@
+# madlibs game
+
+adj1 = input("enter an adjective (description): ")
+noun1 = input("enter a noun (person, place, thing): ")
+noun2 = input("enter another noun (person, place, thing): ")
+adj2 = input("enter another adjective (description): ")
+adj3 = input("enter another adjective (description): ")
+verb1 = input("enter a verb ending with 'ing' (action): ")
+adj4 = input("enter another adjective (description): ")
+noun3 = input("enter another noun (person, place, thing): ")
+verb2 = input("enter another verb ending with 'ing' (action): ")
+verb3 = input("enter another verb ending with 'ing' (action): ")
+adj5 = input("enter another adjective (description): ")
+noun4 = input("enter another noun (person, place, thing): ")
+verb4 = input("enter another verb ending with 'ing' (action): ")
+adj6 = input("enter another adjective (description): ")
+verb5 = input("enter another verb ending with 'ing' (action): ")
+verb6 = input("enter another verb ending with 'ing' (action): ")
+noun5 = input("enter another noun (person, place, thing): ")
+noun6 = input("enter another noun (person, place, thing): ")
+adj7 = input("enter another adjective (description): ")
+
+print(f"\nToday I went to the beach and it was {adj1}! I brought my {noun1} and {noun2} with me. "
+      f"The sun was {adj2}, and the waves were {adj3}. I {verb1} all the way to the water, "
+      f"where I saw a {adj4} {noun3} {verb2} across the sand. "
+      f"My friends and I decided to {verb3} and build a {adj5} sandcastle. "
+      f"Suddenly, a {noun4} {verb4} right next to us! It was so {adj6} that we couldn't stop {verb5}. "
+      f"Finally, as the sun began to {verb6}, we packed up our {noun5} and {noun6} and went home feeling {adj7}.")
