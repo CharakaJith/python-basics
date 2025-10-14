9e2b0a04fad865a8ff4d0b43b7b45c6d5bdfca9a
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-10-15 00:16:30 +0530

    exercise 4 completed

---
+++ exercises/exercise_4.py
@@ -0,0 +1,8 @@
+# calculate circumcenter of a circle
+
+import math
+
+r = float(input("enter the radius of a circle: "))
+circ = round(2 * math.pi * r, 2) # round to 2 decimal places
+
+print(f"\ncircumference of circle (r = {r}): {circ} cm")
