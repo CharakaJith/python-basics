a492574c880d80ffde5432acbc0c54849744b8e7
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-10-15 20:03:32 +0530

    nested loops

---
+++ nested_loops.py
@@ -0,0 +1,8 @@
+# a loop within a loop can be known as a nested loop
+# outer loop:
+#       inner loop:
+
+for x in range(3):
+    for y in range(1, 10):
+        print(y, end=" ") # print x on same line with space between
+    print()
